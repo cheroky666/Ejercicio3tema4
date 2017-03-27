4dec52b788f31dbc3935d5d907c11a7996a491e1
user <user@user-PC>
2017-03-27 08:41:34 +0200

    entregarv2

--- src/ejercicio3v2tema4/Coche.java
+++ src/ejercicio3v2tema4/Coche.java
@@ -20,7 +20,7 @@ public class Coche extends Vehiculotransporte {
     }
 
     /**
-     * constructor por parametros de coche que hereda de vehiculosss.
+     * constructor por parametros de coche que hereda de vehiculosssssss.
      * 
      *
      * @param numeroplazas numero de plazas.
